e4bea4b4e267df6efc9b21b6ebce5fe3ceb84fe9
jecointr <104733115+jecointr@users.noreply.github.com>
2025-11-15 12:52:31 +0100

    completed ex09

---
+++ Python for Data Science/pythonModule00/ex09/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ft_package",
+    version="0.0.1",
+    author="jecointr",
+    author_email="jecointr@42.fr",
+    description="A sample test package",
+    url="https://github.com/jecointr/ft_package",
+    license="MIT",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
